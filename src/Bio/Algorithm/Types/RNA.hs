46b6deddadc6538eaa9960e0059c1122a8c4d766
Ricky Elrod <ricky@elrod.me>
2014-12-13 05:30:27 -0500

    no.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Bio/Algorithm/Types/RNA.hs
+++ src/Bio/Algorithm/Types/RNA.hs
@@ -10,7 +10,7 @@
 -- Stability : experimental
 -- Portability : lens
 --
--- DNA sequences
+-- RNA sequences
 ----------------------------------------------------------------------------
 module Bio.Algorithm.Types.RNA where
 
